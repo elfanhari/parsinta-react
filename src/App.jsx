3b53e43bc4bd5c3b88d948d1283c88998ea0ba4b
elfanhari <elfanhari88@gmail.com>
2025-01-13 20:51:56 +0700

    nested function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import {
-  IconBrandFacebook,
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandTailwind,
-  IconBrandX,
-} from '@tabler/icons-react';
-import clsx from 'clsx';
+import { IconBrandFacebook, IconBrandGithub, IconBrandGoogle, IconBrandTailwind, IconBrandX } from '@tabler/icons-react';
+import Button from './components/Button';
+import Card from './components/Card';
 
-export default function App() {
+function App() {
   return (
-    <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
+    <>
+
+    {/* <div className={'bg-slate-100 grid place-content-center min-h-screen'}>
       <div className={'flex gap-x-2'}>
         <Button
           {...{
@@ -17,8 +14,7 @@ export default function App() {
             onClick: () => alert('This is Tailwind with another style..'),
             className: 'bg-cyan-600',
             type: 'reset',
-          }}
-        >
+          }}>
           <IconBrandTailwind />
           TailwindCSS
         </Button>
@@ -26,38 +22,67 @@ export default function App() {
           <IconBrandFacebook />
           Facebook
         </Button>
-        <Button
-          onClick={() => alert('This is login with X')}
-          className={'bg-pink-600'}
-        >
+        <Button onClick={() => alert('This is login with X')} className={'bg-pink-600'}>
           <IconBrandX />
           X/Twitter
         </Button>
-        <Button
-          type='button'
-          onClick={() => alert('This is login github..')}
-          className={'bg-dark'}
-        >
+        <Button type='button' onClick={() => alert('This is login github..')} className={'bg-black'}>
           <IconBrandGithub />
           GitHub
         </Button>
       </div>
+    </div> */}
+
+    <div className={'bg-slate-100 text-slate-800 tracking-tight antialliased flex items-center justify-center min-h-screen'}>
+      <div className="max-w-2xl w-full flex items-center gap-4">
+        <Card>
+          <Card.Title>
+            Hello React
+          </Card.Title>
+          <Card.Body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, maiores.
+          </Card.Body>
+          <Card.Footer>
+            <Button>Show</Button>
+          </Card.Footer>
+
+        </Card>
+
+        <Card>
+          <Card.Title>
+            Hello React
+          </Card.Title>
+          <Card.Body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, maiores.
+          </Card.Body>
+          <Card.Footer>
+            <Button>Show</Button>
+          </Card.Footer>
+        </Card>
+
+        <Card>
+          <Card.Title>
+            Hello React
+          </Card.Title>
+          <Card.Body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, maiores.
+          </Card.Body>
+          <Card.Footer>
+            <Button>Show</Button>
+          </Card.Footer>
+        </Card>
+      </div>
     </div>
+    </>
   );
 }
 
-function Button(props) {
-  const { type = 'button', className = 'bg-blue-600', children, text } = props;
-  return (
-    <button
-      type={type}
-      {...props}
-      className={clsx(
-        className,
-        '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded',
-      )}
-    >
-      {text || children}
-    </button>
-  );
-}
+const Title = () => {
+  <>
+    <h1>Hello World!</h1>
+    <h2>Hello World 2!</h2>
+  </>;
+};
+
+
+export default App;
